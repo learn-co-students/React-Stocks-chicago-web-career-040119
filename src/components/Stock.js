1f1e32e70beb4b9add4f90bac58451772485e405
Sonia Dumitru <soni.dumitru@gmail.com>
2019-06-15 18:21:29 -0500

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => (
 
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+  <Fragment>
+    <div>
+
+      <div className="card">
+        <div className="card-body" >
+          <h5 className="card-title">{
+              props.stock.name
+            }</h5>
+          <p className="card-text">{
+              props.stock.ticker
+            }</p>
+            <p className="card-text">{
+                props.stock.price
+              }</p>
+            <button onClick={props.parent === 'stock' ?
+              () => props.addToPortofolio(props.stock)
+            : () => props.removeFromPortofolio(props.stock)} >Sell/Buy Stock!</button>
+        </div>
       </div>
+
+
     </div>
-
-
-  </div>
+  </Fragment>
 );
 
 export default Stock
